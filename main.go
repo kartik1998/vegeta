d20e2b30f004913621e753aa587514df53303006
Kartik <kartik@hyperverge.co>
2021-08-28 00:19:43 +0530

    Collect processed system data

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
 )
 
 // ps aux | head -1; ps aux | sort -rnk 4 | head -5
+// https://stackoverflow.com/questions/10781516/how-to-pipe-several-commands-in-go
 
 func main() {
 	data := scripts.CollectProcessData()
-	fmt.Println(data[0])
+	fmt.Println(data[0] + "\n" + data[1])
 }
--- scripts/freez.go
+++ scripts/freez.go
@@ -2,16 +2,30 @@ package scripts
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"os/exec"
 	"strings"
 )
 
+// type Process struct {
+// 	USER    string
+// 	PID     int32
+// 	CPU     float32
+// 	MEM     float32
+// 	VSZ     string
+// 	RSS     string
+// 	TT      string
+// 	STARTED string
+// 	TIME    string
+// 	COMMAND string
+// }
+
 type Process struct {
 	USER    string
 	PID     string
-	CPU     float32
-	MEM     float32
+	CPU     string
+	MEM     string
 	VSZ     string
 	RSS     string
 	TT      string
@@ -33,5 +47,10 @@ func CollectProcessData() []string {
 
 func cleanProcessData(process_data string) []string {
 	data_slice := strings.Split(process_data, "\n")
+	// processed_data_slice := []Process{}
+	for i := 1; i < len(data_slice); i++ {
+		curr_slice := strings.Split(data_slice[i], " ")
+		fmt.Println(curr_slice)
+	}
 	return data_slice
 }
