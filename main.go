bf4568007bc33f8dc945ef5f84b03196ea40403b
Kartik <kartik@hyperverge.co>
2021-08-28 01:48:55 +0530

    Remove redundant comments

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"github.com/kartik1998/freeza/scripts"
 )
 
-// ps aux | head -1; ps aux | sort -rnk 4 | head -5
-// https://stackoverflow.com/questions/10781516/how-to-pipe-several-commands-in-go
-
 func main() {
 	for t := range time.Tick(3 * time.Second) {
 		_, err := scripts.CollectProcessData()
