63b7ecb2ab539cb5cd72f484b7b06c00d6993955
Kartik <kartik@hyperverge.co>
2021-08-28 22:25:24 +0530

    Update: accept snapshot delay from arguements

--- main.go
+++ main.go
@@ -4,17 +4,45 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/kartik1998/vegeta/scripts"
 )
 
+/*
+* @params
+* delayParam: -d
+ */
+var (
+	delay int64 = 1
+)
+
+func init() {
+	for i := 1; i < len(os.Args)-1; i += 2 {
+		param := os.Args[i]
+		var err error
+		if param == "-d" || param == "--delay" {
+			delay, err = strconv.ParseInt(os.Args[i+1], 0, 8)
+			check(err)
+		} else {
+			panic("invalid parameters passed")
+		}
+	}
+}
+
 func main() {
-	for t := range time.Tick(3 * time.Second) {
+	for t := range time.Tick(time.Duration(delay) * time.Second) {
 		_, err := scripts.CollectProcessData()
 		if err != nil {
 			log.Fatal(fmt.Sprintf("%s, %s", t, err))
 			os.Exit(1)
 		}
+	}
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
