2d4df0c786453a9c23523ea59249275f57c3d666
kartik1998 <kartikrawat61@gmail.com>
2021-08-29 12:39:59 +0530

    Add: go-echarts for graph generation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kartik1998/vegeta
 
 go 1.16
+
+require github.com/go-echarts/go-echarts/v2 v2.2.4 // indirect
---
+++ graph/graph.go
@@ -0,0 +1,43 @@
+package graph
+
+import (
+	"math/rand"
+	"os"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
+	"github.com/kartik1998/vegeta/utils"
+)
+
+// generate random data for bar chart
+func generateLineItems() []opts.LineData {
+	items := make([]opts.LineData, 0)
+	for i := 0; i < 7; i++ {
+		items = append(items, opts.LineData{Value: rand.Intn(300)})
+	}
+	return items
+}
+
+func GenerateGraph() {
+	line := charts.NewLine()
+	// set some global options like Title/Legend/ToolTip or anything else
+	line.SetGlobalOptions(
+		charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+		charts.WithTitleOpts(opts.Title{
+			Title:    "Vegeta graph",
+			Subtitle: utils.Time(),
+		}))
+
+	// Put data into instance
+	line.SetXAxis([]string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}).
+		AddSeries("Category A", generateLineItems()).
+		AddSeries("Category B", generateLineItems()).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+	// Where the magic happens
+	f, err := os.Create(utils.Time() + ".html")
+	if err != nil {
+		panic(err)
+	}
+	line.Render(f)
+}
